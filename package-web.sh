7cd5b73bce6d39caefe6a62e4eed14f3888b8f48
Bin-Huang <tohuangbin@gmail.com>
2023-07-10 21:01:06 +0800

    chore: 新增 npm 脚本 package:web

---
+++ package-web.sh
@@ -0,0 +1,29 @@
+set -e
+
+
+npm install
+
+rm -rf release/app/dist/renderer
+npm run build:renderer
+
+_imageName="ccr.ccs.tencentyun.com/wheremylife/chatbox-web"
+_version=`git rev-parse --short HEAD`
+_imageTag="$_imageName:$_version"
+
+echo "image tag: $_imageTag"
+
+echo
+
+echo "start docker build: $_imageTag"
+docker build . -f Dockerfile -t $_imageTag
+
+echo
+
+echo "start docker push: $_imageTag"
+docker push $_imageTag
+
+echo
+
+echo "======================================"
+echo $_imageTag
+echo "======================================"
