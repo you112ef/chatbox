e98be92c035f622dd56f92c1bc106369ffc83d36
Bin-Huang <tohuangbin@gmail.com>
2023-08-17 11:01:16 +0800

    chore: 小屏幕下窗口显示时不再自动聚焦输入框

--- src/renderer/hooks/useShortcut.tsx
+++ src/renderer/hooks/useShortcut.tsx
@@ -1,12 +1,19 @@
 import { useEffect } from 'react'
 import * as api from '../packages/runtime'
 import * as dom from './dom'
+import * as atoms from '../stores/atoms'
 import * as sessionActions from '../stores/sessionActions'
+import { getDefaultStore } from 'jotai'
 
 export default function useShortcut() {
     useEffect(() => {
         api.onWindowShow(() => {
-            dom.focusMessageInput()
+            const store = getDefaultStore()
+            const isSmallScreen = store.get(atoms.isSmallScreenAtom)
+            // 大屏幕下，窗口显示时自动聚焦输入框
+            if (! isSmallScreen) {
+                dom.focusMessageInput()
+            }
         })
 
         window.addEventListener('keydown', keyboardShortcut)
