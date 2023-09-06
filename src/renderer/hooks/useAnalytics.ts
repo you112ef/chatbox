413f16be824c209d33324c73b459984b4aaf2ed0
Bin-Huang <tohuangbin@gmail.com>
2023-09-06 15:31:24 +0800

    fix: 修复 react-ga4 的注册问题

--- src/renderer/hooks/useAnalytics.ts
+++ src/renderer/hooks/useAnalytics.ts
@@ -6,27 +6,33 @@ import ReactGA from 'react-ga4'
 function useAnalytics() {
     // GA4
     useEffect(() => {
-        ;(async () => {
+        ; (async () => {
             const GAID = 'G-B365F44W6E'
             try {
                 const conf = await getConfig()
-                const version = await api.getVersion()
-                const platfrom = await api.getPlatform()
+                const options = {
+                    app_version: await api.getVersion(),
+                    chatbox_platform: await api.getPlatform(),
+                    clientId: conf.uuid,
+                    userId: conf.uuid,
+                }
                 ReactGA.initialize([
                     {
                         trackingId: GAID,
-                        gaOptions: {
-                            app_version: version,
-                            clientId: conf.uuid,
-                            chatbox_platform: platfrom,
-                        },
-                        gtagOptions: {
-                            app_version: version,
-                            clientId: conf.uuid,
-                            chatbox_platform: platfrom,
-                        },
+                        gaOptions: options,
+                        gtagOptions: options,
                     },
                 ])
+                ReactGA.set(options)
+                // https://github.com/codler/react-ga4/issues/7
+                ReactGA.gtag('config', GAID, {
+                    'user_id': conf.uuid,
+                });
+                ReactGA.send({
+                    hitType: 'pageview',
+                    page: window.location.pathname + window.location.search,
+        			user_id: conf.uuid,
+                })
             } catch (e) {
                 ReactGA.initialize(GAID)
                 throw e
