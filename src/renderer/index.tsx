cd4e94bca9f4507d42ee55b41be09e16f9b2f39b
Bin-Huang <tohuangbin@gmail.com>
2023-09-15 15:12:59 +0800

    chore: 减少 sentry 样本比例

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -19,10 +19,11 @@ import './static/index.css'
             new Sentry.Replay(),
         ],
         // Performance Monitoring
-        tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+        sampleRate: 0.10,
+        tracesSampleRate: 0.10, // Capture 100% of the transactions, reduce in production!
         // Session Replay
-        replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-        replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+        replaysSessionSampleRate: 0.05, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+        replaysOnErrorSampleRate: 0.05, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
         release: version,
     })
 })()
