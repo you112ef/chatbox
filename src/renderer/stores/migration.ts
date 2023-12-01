e08655dc9c2e6880596408c97f554c8fd832c564
Bin-Huang <tohuangbin@gmail.com>
2023-12-01 16:48:55 +0800

    tweak: 修复 migrate 水合时间过短导致的数据丢失问题

--- src/renderer/stores/migration.ts
+++ src/renderer/stores/migration.ts
@@ -5,7 +5,7 @@ import * as runtime from '@/packages/runtime'
 
 export function migrate() {
     // 通过定时器延迟启动，防止处理状态底层存储的异步加载前错误的初始数据（水合阶段）
-    setTimeout(_migrate, 5000)
+    setTimeout(_migrate, 2000)
 }
 
 async function _migrate() {
