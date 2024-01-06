96fde647834917dbaf2343d93109a303141372c6
Bin-Huang <tohuangbin@gmail.com>
2024-01-06 23:10:10 +0800

    fix: 修复 iOS 16及以前的白屏问题

--- src/renderer/setup/mobile_browser_viewport_height.ts
+++ src/renderer/setup/mobile_browser_viewport_height.ts
@@ -6,6 +6,28 @@ function setViewportHeight() {
 }
 setViewportHeight()
 
+// 监听屏幕旋转时，更新视口高度
 window.addEventListener('resize', setViewportHeight)
 window.addEventListener('orientationchange', setViewportHeight)
-screen.orientation.addEventListener('change', setViewportHeight)
+if (screen && screen.orientation) {
+    screen.orientation.addEventListener('change', setViewportHeight)
+} else {
+    // 兼容 iOS 16 及更低版本
+    const mql = window.matchMedia('(orientation: portrait)');
+    if (mql) {
+        mql.addListener(function (m) {
+            setViewportHeight()
+            // if (m.matches) {
+            //     // 当前为竖屏模式
+            // } else {
+            //     // 当前为横屏模式
+            // }
+        });
+        // // 用来检查当前的媒体查询状态
+        // if (mql.matches) {
+        //     // 当前为竖屏模式
+        // } else {
+        //     // 当前为横屏模式
+        // }
+    }
+}
