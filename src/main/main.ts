05faa46da0041c7773e8aea30136c61c53b1c0e4
Bin-Huang <tohuangbin@gmail.com>
2024-01-12 16:27:52 +0800

    chore: electron 开发环境不加载浏览器插件

--- src/main/main.ts
+++ src/main/main.ts
@@ -71,22 +71,23 @@ if (isDebug) {
     require('electron-debug')()
 }
 
-const installExtensions = async () => {
-    const installer = require('electron-devtools-installer')
-    const forceDownload = !!process.env.UPGRADE_EXTENSIONS
-    const extensions = ['REACT_DEVELOPER_TOOLS']
+// const installExtensions = async () => {
+//     const installer = require('electron-devtools-installer')
+//     const forceDownload = !!process.env.UPGRADE_EXTENSIONS
+//     const extensions = ['REACT_DEVELOPER_TOOLS']
 
-    return installer
-        .default(
-            extensions.map((name) => installer[name]),
-            forceDownload
-        )
-        .catch(console.log)
-}
+//     return installer
+//         .default(
+//             extensions.map((name) => installer[name]),
+//             forceDownload
+//         )
+//         .catch(console.log)
+// }
 
 const createWindow = async () => {
     if (isDebug) {
-        await installExtensions()
+        // 不在安装 DEBUG 浏览器插件。可能不兼容，所以不如直接在网页里debug
+        // await installExtensions()
     }
 
     const RESOURCES_PATH = app.isPackaged
