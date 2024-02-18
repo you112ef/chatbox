f319493591a65981d60ed0371f11a36ed8ffe61a
Bin-Huang <tohuangbin@gmail.com>
2024-02-18 20:59:12 +0800

    chore: 弃用 releases.chatboxai.app 接口地址

--- src/renderer/packages/remote.ts
+++ src/renderer/packages/remote.ts
@@ -10,7 +10,7 @@ import { ofetch } from 'ofetch'
 
 // ========== API ORIGIN 根据速度维护 ==========
 
-const RELEASE_ORIGIN = 'https://releases.chatboxai.app'
+// const RELEASE_ORIGIN = 'https://releases.chatboxai.app'
 
 export let API_ORIGIN = 'https://chatboxai.app'
 
@@ -45,7 +45,8 @@ export async function checkNeedUpdate(version: string, os: string, config: Confi
     type Response = {
         need_update?: boolean
     }
-    const res = await ofetch<Response>(`${RELEASE_ORIGIN}/chatbox_need_update/${version}`, {
+    // const res = await ofetch<Response>(`${RELEASE_ORIGIN}/chatbox_need_update/${version}`, {
+    const res = await ofetch<Response>(`${API_ORIGIN}/chatbox_need_update/${version}`, {
         method: 'POST',
         retry: 3,
         body: {
@@ -60,7 +61,8 @@ export async function getSponsorAd(): Promise<null | SponsorAd> {
     type Response = {
         data: null | SponsorAd
     }
-    const res = await ofetch<Response>(`${RELEASE_ORIGIN}/sponsor_ad`, {
+    // const res = await ofetch<Response>(`${RELEASE_ORIGIN}/sponsor_ad`, {
+    const res = await ofetch<Response>(`${API_ORIGIN}/sponsor_ad`, {
         retry: 3,
     })
     return res['data'] || null
@@ -70,7 +72,8 @@ export async function listSponsorAboutBanner() {
     type Response = {
         data: SponsorAboutBanner[]
     }
-    const res = await ofetch<Response>(`${RELEASE_ORIGIN}/sponsor_about_banner`, {
+    // const res = await ofetch<Response>(`${RELEASE_ORIGIN}/sponsor_about_banner`, {
+    const res = await ofetch<Response>(`${API_ORIGIN}/sponsor_ad`, {
         retry: 3,
     })
     return res['data']
