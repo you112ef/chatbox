1940b017bf8b8ef783384bf1f6f83e0ac399de79
Benn Huang <tohuangbin@gmail.com>
2024-03-20 10:45:23 +0800

    feat: 为标题栏追加编辑图标；抽离标题栏组件 (#69)

--- src/renderer/MainPane.tsx
+++ src/renderer/MainPane.tsx
@@ -1,32 +1,22 @@
 import { useEffect } from 'react'
-import { Box, IconButton, ButtonGroup, Stack, Typography, Chip, Tooltip, useTheme } from '@mui/material'
-import { isChatSession, isPictureSession } from '../shared/types'
-import { useTranslation } from 'react-i18next'
+import { Box, IconButton, ButtonGroup } from '@mui/material'
 import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp'
 import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown'
 import SponsorChip from './components/SponsorChip'
 import * as atoms from './stores/atoms'
-import { useAtom, useAtomValue, useSetAtom } from 'jotai'
+import { useAtom, useAtomValue } from 'jotai'
 import InputBox from './components/InputBox'
 import MessageList from './components/MessageList'
-import * as sessionActions from './stores/sessionActions'
 import * as scrollActions from './stores/scrollActions'
-import TuneIcon from '@mui/icons-material/Tune'
-import MenuOpenIcon from '@mui/icons-material/MenuOpen'
 import { drawerWidth } from './Sidebar'
-import ImageIcon from '@mui/icons-material/Image'
-import Toolbar from './components/Toolbar'
-import { getModelDisplayName } from '@/packages/models'
 import { useIsSmallScreen } from './hooks/useScreenChange'
+import Header from './components/Header'
 
 interface Props {}
 
 export default function MainPane(props: Props) {
-    const { t } = useTranslation()
-    const theme = useTheme()
     const currentSession = useAtomValue(atoms.currentSessionAtom)
-    const setChatConfigDialogSession = useSetAtom(atoms.chatConfigDialogAtom)
-    const [showSidebar, setShowSidebar] = useAtom(atoms.showSidebarAtom)
+    const [showSidebar] = useAtom(atoms.showSidebarAtom)
 
     const atScrollTop = useAtomValue(atoms.messageScrollingAtTopAtom)
     const atScrollBottom = useAtomValue(atoms.messageScrollingAtBottomAtom)
@@ -37,20 +27,6 @@ export default function MainPane(props: Props) {
             scrollActions.scrollToBottom('auto') // 每次启动时自动滚动到底部
         }, 200)
     }, [])
-
-    // 会话名称自动生成
-    useEffect(() => {
-        if (
-            currentSession.name === 'Untitled' &&
-            currentSession.messages.findIndex((msg) => msg.role === 'assistant' && !msg.generating) !== -1
-        ) {
-            sessionActions.generateName(currentSession.id)
-        }
-    }, [currentSession.messages])
-
-    const editCurrentSession = () => {
-        setChatConfigDialogSession(currentSession)
-    }
 
     return (
         <Box
@@ -71,88 +47,7 @@ export default function MainPane(props: Props) {
                         </Box>
                     )
                 }
-                <div
-                    className="flex flex-row pt-3 pb-2 px-0.5 sm:px-4"
-                    style={{
-                        borderBottomWidth: '1px',
-                        borderBottomStyle: 'solid',
-                        borderBottomColor: theme.palette.divider,
-                    }}
-                >
-                    {!showSidebar && (
-                        <Box className="mr-1">
-                            <IconButton onClick={() => setShowSidebar(!showSidebar)}>
-                                <MenuOpenIcon className="text-xl" sx={{ transform: 'rotate(180deg)' }} />
-                            </IconButton>
-                        </Box>
-                    )}
-                    <Typography
-                        variant="h6"
-                        color="inherit"
-                        component="div"
-                        noWrap
-                        sx={{
-                            flex: 1,
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                        }}
-                        className="flex items-center cursor-pointer"
-                        onClick={() => {
-                            editCurrentSession()
-                        }}
-                    >
-                        {!showSidebar ? (
-                            <>
-                                <Typography variant="h6" noWrap className="ml-1 max-w-56">
-                                    {currentSession.name}
-                                </Typography>
-                            </>
-                        ) : (
-                            <Typography variant="h6" noWrap className="ml-3 max-w-56">
-                                {currentSession.name}
-                            </Typography>
-                        )}
-                        {isChatSession(currentSession) && currentSession.settings && (
-                            <Tooltip
-                                title={t('Current conversation configured with specific model settings')}
-                                className="cursor-pointer"
-                            >
-                                <Chip
-                                    className="ml-2 cursor-pointer"
-                                    variant="outlined"
-                                    color="warning"
-                                    size="small"
-                                    icon={<TuneIcon className="cursor-pointer" />}
-                                    label={
-                                        <span className="cursor-pointer">
-                                            {getModelDisplayName(currentSession.settings, currentSession.type)}
-                                        </span>
-                                    }
-                                />
-                            </Tooltip>
-                        )}
-                        {isPictureSession(currentSession) && (
-                            <Tooltip
-                                title={t('The Image Creator plugin has been activated for the current conversation')}
-                                className="cursor-pointer"
-                            >
-                                <Chip
-                                    className="ml-2 cursor-pointer"
-                                    variant="outlined"
-                                    color="secondary"
-                                    size="small"
-                                    icon={<ImageIcon className="cursor-pointer" />}
-                                    label={<span className="cursor-pointer">{t('Image Creator')}</span>}
-                                />
-                            </Tooltip>
-                        )}
-                    </Typography>
-                    {
-                        // 大屏幕的广告UI
-                        !isSmallScreen && <SponsorChip sessionId={currentSession.id} />
-                    }
-                    <Toolbar />
-                </div>
+                <Header />
                 <MessageList />
                 <Box className="relative">
                     <ButtonGroup
---
+++ src/renderer/components/Header.tsx
@@ -0,0 +1,131 @@
+import { useEffect } from 'react'
+import { Box, IconButton, Typography, Chip, Tooltip, useTheme } from '@mui/material'
+import { isChatSession, isPictureSession } from '../../shared/types'
+import { useTranslation } from 'react-i18next'
+import SponsorChip from './SponsorChip'
+import * as atoms from '../stores/atoms'
+import { useAtom, useAtomValue, useSetAtom } from 'jotai'
+import * as sessionActions from '../stores/sessionActions'
+import TuneIcon from '@mui/icons-material/Tune'
+import MenuOpenIcon from '@mui/icons-material/MenuOpen'
+import ImageIcon from '@mui/icons-material/Image'
+import Toolbar from './Toolbar'
+import { getModelDisplayName } from '@/packages/models'
+import { useIsSmallScreen } from '../hooks/useScreenChange'
+import { Pencil } from 'lucide-react'
+import { cn } from '@/lib/utils'
+
+interface Props { }
+
+export default function Header(props: Props) {
+    const { t } = useTranslation()
+    const theme = useTheme()
+    const currentSession = useAtomValue(atoms.currentSessionAtom)
+    const setChatConfigDialogSession = useSetAtom(atoms.chatConfigDialogAtom)
+    const [showSidebar, setShowSidebar] = useAtom(atoms.showSidebarAtom)
+
+    const isSmallScreen = useIsSmallScreen()
+
+    // 会话名称自动生成
+    useEffect(() => {
+        if (
+            currentSession.name === 'Untitled' &&
+            currentSession.messages.findIndex((msg) => msg.role === 'assistant' && !msg.generating) !== -1
+        ) {
+            sessionActions.generateName(currentSession.id)
+        }
+    }, [currentSession.messages])
+
+    const editCurrentSession = () => {
+        setChatConfigDialogSession(currentSession)
+    }
+
+    let EditButton: React.ReactNode | null = null
+    if (isChatSession(currentSession) && currentSession.settings) {
+        EditButton = (
+            <Tooltip
+                title={t('Current conversation configured with specific model settings')}
+                className="cursor-pointer"
+            >
+                <Chip
+                    className="ml-2 cursor-pointer"
+                    variant="outlined"
+                    color="warning"
+                    size="small"
+                    icon={<TuneIcon className="cursor-pointer" />}
+                    label={
+                        <span className="cursor-pointer">
+                            {getModelDisplayName(currentSession.settings, currentSession.type)}
+                        </span>
+                    }
+                />
+            </Tooltip>
+        )
+    } else if (isPictureSession(currentSession)) {
+        EditButton = (
+            <Tooltip
+                title={t('The Image Creator plugin has been activated for the current conversation')}
+                className="cursor-pointer"
+            >
+                <Chip
+                    className="ml-2 cursor-pointer"
+                    variant="outlined"
+                    color="secondary"
+                    size="small"
+                    icon={<ImageIcon className="cursor-pointer" />}
+                    label={<span className="cursor-pointer">{t('Image Creator')}</span>}
+                />
+            </Tooltip>
+        )
+    } else {
+        EditButton = (
+            <Pencil className="ml-1 cursor-pointer opacity-30 w-4 h-4" fontSize="small" />
+        )
+    }
+
+    return (
+        <div
+            className="flex flex-row pt-3 pb-2 px-0.5 sm:px-4"
+            style={{
+                borderBottomWidth: '1px',
+                borderBottomStyle: 'solid',
+                borderBottomColor: theme.palette.divider,
+            }}
+        >
+            {!showSidebar && (
+                <Box className="mr-1">
+                    <IconButton onClick={() => setShowSidebar(!showSidebar)}>
+                        <MenuOpenIcon className="text-xl" sx={{ transform: 'rotate(180deg)' }} />
+                    </IconButton>
+                </Box>
+            )}
+            <Typography
+                variant="h6"
+                color="inherit"
+                component="div"
+                noWrap
+                sx={{
+                    flex: 1,
+                    overflow: 'hidden',
+                    textOverflow: 'ellipsis',
+                }}
+                className="flex items-center cursor-pointer"
+                onClick={() => {
+                    editCurrentSession()
+                }}
+            >
+                {
+                    <Typography variant="h6" noWrap className={cn('max-w-56', showSidebar ? 'ml-3' : 'ml-1')}>
+                        {currentSession.name}
+                    </Typography>
+                }
+                {EditButton}
+            </Typography>
+            {
+                // 大屏幕的广告UI
+                !isSmallScreen && <SponsorChip sessionId={currentSession.id} />
+            }
+            <Toolbar />
+        </div>
+    )
+}
