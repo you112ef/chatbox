4ebb5dafe0085284a5c4f09cdc150735fdf23751
Bin-Huang <tohuangbin@gmail.com>
2024-06-16 21:18:48 +0800

    feat: 为 ollama 的配置添加提示

--- src/renderer/pages/SettingDialog/OllamaSetting.tsx
+++ src/renderer/pages/SettingDialog/OllamaSetting.tsx
@@ -1,9 +1,12 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { Alert, FormControl, InputLabel, MenuItem, Select } from '@mui/material'
 import { ModelSettings } from '../../../shared/types'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 import TextFieldReset from '@/components/TextFieldReset'
 import { useEffect, useState } from 'react'
 import Ollama from '@/packages/models/ollama'
+import platform from '@/platform'
+import { useAtomValue } from 'jotai'
+import { languageAtom } from '@/stores/atoms'
 
 export function OllamaHostInput(props: {
     ollamaHost: string
@@ -11,15 +14,34 @@ export function OllamaHostInput(props: {
     className?: string
 }) {
     const { t } = useTranslation()
+    const language = useAtomValue(languageAtom)
     return (
-        <TextFieldReset
-            label={t('api host')}
-            value={props.ollamaHost}
-            defaultValue='http://localhost:11434'
-            onValueChange={props.setOllamaHost}
-            fullWidth
-            className={props.className}
-        />
+        <>
+            <TextFieldReset
+                label={t('api host')}
+                value={props.ollamaHost}
+                defaultValue='http://localhost:11434'
+                onValueChange={props.setOllamaHost}
+                fullWidth
+                className={props.className}
+            />
+            {
+                props.ollamaHost
+                && props.ollamaHost.length > 16
+                && !props.ollamaHost.includes('localhost')
+                && !props.ollamaHost.includes('127.0.0.1') && (
+                    <Alert icon={false} severity='info' className='my-4'>
+                        <Trans i18nKey='Please ensure that the Remote Ollama Service is able to connect remotely. For more details, refer to <a>this tutorial</a>.'
+                            components={{
+                                a: <a className='cursor-pointer font-bold' onClick={() => {
+                                    platform.openLink(`https://chatboxai.app/redirect_app/ollama_guide/${language}`)
+                                }}></a>,
+                            }}
+                        />
+                    </Alert>
+                )
+            }
+        </>
     )
 }
 
