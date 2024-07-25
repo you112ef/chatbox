381220086198aef7ca575d32b9876707cb000014
Bin-Huang <tohuangbin@gmail.com>
2024-07-25 14:32:18 +0800

    refactor: 升级到 capacitor 6

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,14 @@ const config: CapacitorConfig = {
     appId: 'xyz.chatboxapp.chatbox',
     appName: 'Chatbox',
     webDir: 'release/app/dist/renderer',
-    // 解决 Android 无法访问 http 服务的问题（比如连接局域网中的 Ollama 服务）
-    // https://github.com/ionic-team/capacitor/discussions/4477
     server: {
-        cleartext: true
+        // 解决 Android 无法访问 http 服务的问题（比如连接局域网中的 Ollama 服务）
+        // https://github.com/ionic-team/capacitor/discussions/4477
+        cleartext: true,
+
+        // 从 capacitor5 升级到 6 之后，androidScheme 默认为 https。这会导致历史数据无法访问（因为 domain 发生了改变），因此需要手动设置回 http。
+        // https://capacitorjs.com/docs/updating/6-0#update-androidscheme
+        androidScheme: "http"
     },
     ios: {
         scheme: 'Chatbox AI',
