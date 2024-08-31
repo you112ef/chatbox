0aebe955f43d96fbc86fb601843b16d4793df221
Bin-Huang <tohuangbin@gmail.com>
2024-08-31 14:24:05 +0800

    revert: Revert "chore: 网页版自动适应系统语言"
    This reverts commit bc735aabd5a7dab038a36d1902df7a7dd35cffee.

--- src/renderer/hooks/useDefaultSystemLanguage.ts
+++ src/renderer/hooks/useDefaultSystemLanguage.ts
@@ -12,6 +12,14 @@ export function useSystemLanguageWhenInit() {
                 const settings = store.get(settingsAtom)
                 if (!settings.languageInited) {
                     let locale = await platform.getLocale()
+
+                    // 网页版暂时不自动更改简体中文，防止网址封禁
+                    if (platform.type === 'web') {
+                        if (locale === 'zh-Hans') {
+                            locale = 'en'
+                        }
+                    }
+
                     settings.language = locale
                 }
                 settings.languageInited = true
