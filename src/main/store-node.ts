364d399a0c3eba39919ff6296062da38b3cd8811
Benn Huang <tohuangbin@gmail.com>
2024-09-02 17:31:24 +0800

    fix: 修复因 config.json 文件 JSON 异常格式导致的数据丢失问题 (#99)

--- src/main/store-node.ts
+++ src/main/store-node.ts
@@ -5,22 +5,62 @@ import * as defaults from '../shared/defaults'
 import path from 'path'
 import { app } from 'electron'
 import * as fs from 'fs-extra'
+import { powerMonitor } from 'electron'
 
+const configPath = path.resolve(app.getPath('userData'), 'config.json')
+
+// 1) 检查配置文件是否合法
+// 如果配置文件不合法，则使用最新的备份文件
+if (fs.existsSync(configPath) && !checkConfigValid(configPath)) {
+    log.error('store-node: config.json is invalid.')
+    const backups = getBackups()
+    if (backups.length > 0) {
+        // 不断尝试使用最新的备份文件，直到成功
+        for (let i = backups.length - 1; i >= 0; i--) {
+            const backup = backups[i]
+            if (checkConfigValid(backup.filepath)) {
+                fs.copySync(backup.filepath, configPath)
+                log.info('store-node: use backup:', backup.filepath)
+                break
+            }
+        }
+    }
+}
+
+// 2) 初始化store
 interface StoreType {
     settings: Settings
     configs: Config
     lastShownAboutDialogVersion: string // 上次启动时自动弹出关于对话框的应用版本
 }
-
 export const store = new Store<StoreType>({
     clearInvalidConfig: true, // 当配置JSON不合法时，清空配置
 })
-log.info('store path:', store.path)
+log.info('store-node: init store, config path:', store.path)
 
-// 自动备份
-autoBackup().catch((err) => {
-    log.error('auto backup error:', err)
+// 3) 启动自动备份，每10分钟备份一次，并自动清理多余的备份文件
+autoBackup()
+let autoBackupTimer = setInterval(autoBackup, 10 * 60 * 1000)
+powerMonitor.on('resume', () => {
+    clearInterval(autoBackupTimer)
+    autoBackupTimer = setInterval(autoBackup, 10 * 60 * 1000)
 })
+powerMonitor.on('suspend', () => {
+    clearInterval(autoBackupTimer)
+})
+async function autoBackup() {
+    try {
+        if (needBackup()) {
+            const filename = await backup()
+            if (filename) {
+                log.info('store-node: auto backup:', filename)
+            }
+        }
+        await clearBackups()
+    } catch (err) {
+        log.error('store-node: auto backup error:', err)
+    }
+}
 
 export function getSettings(): Settings {
     const settings = store.get<'settings'>('settings', defaults.settings())
@@ -36,45 +76,47 @@ export function getConfig(): Config {
     return configs
 }
 
-// ------------------ backup ------------------
-
+/**
+ * 备份配置文件
+ */
 export async function backup() {
-    const configPath = path.resolve(app.getPath('userData'), 'config.json')
     if (!fs.existsSync(configPath)) {
+        log.error('store-node: skip backup because config.json does not exist.')
         return
     }
-    const now = new Date()
-    const year = now.getFullYear()
-    const month = String(now.getMonth() + 1).padStart(2, '0')
-    const day = String(now.getDate()).padStart(2, '0')
-    const backupPath = path.resolve(app.getPath('userData'), `config-backup-${year}-${month}-${day}.json`)
+    if (!checkConfigValid(configPath)) {
+        log.error('store-node: skip backup because config.json is invalid.')
+        return
+    }
+    let now = new Date().toISOString().replace(/:/g, '_')
+    const backupPath = path.resolve(app.getPath('userData'), `config-backup-${now}.json`)
     try {
         await fs.copy(configPath, backupPath)
     } catch (err) {
-        log.error('Failed to backup config:', err)
+        log.error('store-node: Failed to backup config:', err)
         return
     }
+    log.info('store-node: backup config to:', backupPath)
     return backupPath
 }
 
-export async function getBackups() {
-    let filenames: string[]
-    try {
-        filenames = await fs.readdir(app.getPath('userData'))
-    } catch (err) {
-        log.error('Failed to read directory:', err)
-        return []
-    }
+/**
+ * 获取所有备份文件，并按照时间排序
+ * @returns 备份文件信息
+ */
+export function getBackups() {
+    const filenames = fs.readdirSync(app.getPath('userData'))
     const backupFilenames = filenames.filter((filename) => filename.startsWith('config-backup-'))
     if (backupFilenames.length === 0) {
         return []
     }
     let backupFileInfos = backupFilenames.map((filename) => {
-        const dateStr = filename.replace('config-backup-', '').replace('.json', '')
+        let dateStr = filename.replace('config-backup-', '').replace('.json', '')
+        dateStr = dateStr.replace(/_/g, ':')
         const date = new Date(dateStr)
         return {
             filename,
-            // date,
+            filepath: path.resolve(app.getPath('userData'), filename),
             dateMs: date.getTime() || 0,
         }
     })
@@ -82,42 +124,50 @@ export async function getBackups() {
     return backupFileInfos
 }
 
-export async function needBackup() {
-    // 7天备份一次
-    const backups = await getBackups()
+/**
+ * 检查是否需要备份
+ * @returns 是否需要备份
+ */
+export function needBackup() {
+    const backups = getBackups()
     if (backups.length === 0) {
         return true
     }
     const lastBackup = backups[backups.length - 1]
-    return lastBackup.dateMs < Date.now() - 7 * 24 * 60 * 60 * 1000
+    return lastBackup.dateMs < Date.now() - 10 * 60 * 1000 // 10分钟备份一次
 }
 
+/**
+ * 清理备份文件，仅保留最近50个备份
+ */
 export async function clearBackups() {
-    // 保留最近10个备份
-    const backups = await getBackups()
-    if (backups.length < 10) {
+    const limit = 50
+    const backups = getBackups()
+    if (backups.length < limit) {
         return
     }
-    const needDelete = backups.slice(0, backups.length - 10)
+    const needDelete = backups.slice(0, backups.length - limit)
     try {
         await Promise.all(
             needDelete.map(async (backup) => {
-                const backupPath = path.resolve(app.getPath('userData'), backup.filename)
-                await fs.remove(backupPath)
-                log.info('clear backup:', backup.filename)
+                await fs.remove(backup.filepath)
+                log.info('store-node: clear backup:', backup.filename)
             })
         )
     } catch (err) {
-        log.error('Failed to clear backups:', err)
+        log.error('store-node: Failed to clear backups:', err)
     }
 }
 
-export async function autoBackup() {
-    if (await needBackup()) {
-        const filename = await backup()
-        if (filename) {
-            log.info('auto backup:', filename)
-        }
+/**
+ * 检查配置文件是否是合法的JSON文件
+ * @returns 配置文件是否合法
+ */
+function checkConfigValid(filepath: string) {
+    try {
+        JSON.parse(fs.readFileSync(filepath, 'utf8'))
+    } catch (err) {
+        return false
     }
-    await clearBackups()
+    return true
 }
