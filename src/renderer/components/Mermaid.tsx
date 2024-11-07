3f7fc16576faab8cbe0529a3bd91733252115ab4
Bin-Huang <tohuangbin@gmail.com>
2024-11-07 21:29:21 +0800

    fix: 修复了 mermaid 处理复杂流程图时文字不完整的问题

--- src/renderer/components/Mermaid.tsx
+++ src/renderer/components/Mermaid.tsx
@@ -14,14 +14,16 @@ export function MessageMermaid(props: {
 }) {
     const { source, theme, generating } = props
 
+    const [svgId, setSvgId] = useState('')
     const [svgCode, setSvgCode] = useState('')
     useEffect(() => {
         if (generating) {
             return
         }
         (async () => {
-            const svgCode = await mermaidCodeToSvgCode(source, theme)
-            setSvgCode(svgCode)
+            const { id, svg } = await mermaidCodeToSvgCode(source, theme)
+            setSvgCode(svg)
+            setSvgId(id)
         })()
     }, [source, theme, generating])
 
@@ -32,7 +34,8 @@ export function MessageMermaid(props: {
     }
 
     return (
-        <SVGPreview xmlCode={svgCode} />
+        // <SVGPreview xmlCode={svgCode} />
+        <SVGPreviewDangerous svgId={svgId} xmlCode={svgCode} />
     )
 }
 
@@ -44,6 +47,45 @@ export function Loading() {
         </div>
     )
 }
+
+/**
+ * 直接将 svg 代码注入到页面中，通过浏览器自身的修复能力处理 svg 代码，再通过 serializeToString 得到规范的 svg 代码。
+ * 经过各种测试，发现有时候 mermaid 生成的 svg 代码并不规范，直接转化 base64 将无法完整显示。
+ * 这里的做法是直接将 svg 代码注入到页面中，通过浏览器自身的修复能力处理 svg 代码，再通过 serializeToString 得到规范的 svg 代码。
+ */
+export function SVGPreviewDangerous(props: {
+    xmlCode: string
+    svgId: string
+    className?: string
+    generating?: boolean
+}) {
+    const { svgId, xmlCode, className, generating } = props
+    const setPictureShow = useSetAtom(atoms.pictureShowAtom)
+    if (!xmlCode.includes('</svg') && generating) {
+        return <Loading />
+    }
+    return (
+        <div
+            className={cn("cursor-pointer my-2", className)}
+            onClick={async () => {
+                const svg = document.getElementById(svgId)
+                if (!svg) {
+                    return
+                }
+                const serializedSvgCode = new XMLSerializer().serializeToString(svg)
+                const base64 = svgCodeToBase64(serializedSvgCode)
+                const pngBase64 = await svgToPngBase64(base64)
+                setPictureShow({
+                    picture: { url: pngBase64 },
+                })
+            }}
+        >
+            {/* 这里直接注入了 svg 代码 */}
+            <div dangerouslySetInnerHTML={{ __html: xmlCode }} />
+        </div>
+    )
+}
+
 
 export function SVGPreview(props: {
     xmlCode: string
@@ -87,11 +129,13 @@ export function SVGPreview(props: {
 
 async function mermaidCodeToSvgCode(source: string, theme: 'light' | 'dark') {
     mermaid.initialize({ theme: theme === 'light' ? 'default' : 'dark' })
-    const result = await mermaid.render('mermaidtmp', source)
+    const id = 'mermaidtmp' + Math.random().toString(36).substring(2, 15)
+    const result = await mermaid.render(id, source)
+    // 考虑到 mermaid 工具内部本身已经使用了 dompurify 进行处理，因此可以先假设它的输出是安全的
     // 经过测试，发现 dompurify.sanitize 有时候会导致最终的 svg 显示不完整
     // 考虑到现代浏览器都不会执行 svg 中的 script 标签，所以这里不进行 sanitize。参考：https://stackoverflow.com/questions/7917008/xss-when-loading-untrusted-svg-using-img-tag
     // return dompurify.sanitize(result.svg, { USE_PROFILES: { svg: true, svgFilters: true } })
-    return result.svg
+    return { id, svg: result.svg }
 }
 
 function svgCodeToBase64(svgCode: string) {
