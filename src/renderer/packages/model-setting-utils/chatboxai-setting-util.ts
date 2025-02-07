fa9cb256f98105dbbd392e2688bc4f9172c40531
Bin-Huang <tohuangbin@gmail.com>
2025-02-07 19:40:19 +0800

    chore: chatbox ai 模型选择器中为中文用户默认提供 deepseek 选项

--- src/renderer/packages/model-setting-utils/chatboxai-setting-util.ts
+++ src/renderer/packages/model-setting-utils/chatboxai-setting-util.ts
@@ -2,6 +2,7 @@ import { ChatboxAIModel, ModelSettings, Session, SessionType, Settings } from 's
 import { ModelSettingUtil } from './interface'
 import { chatboxAIModels } from '../models/chatboxai'
 import BaseConfig from './base-config'
+import { ModelOptionGroup } from 'src/shared/types'
 
 export default class ChatboxAISettingUtil extends BaseConfig implements ModelSettingUtil {
     async getCurrentModelDisplayName(settings: Settings, sessionType: SessionType): Promise<string> {
@@ -23,9 +24,13 @@ export default class ChatboxAISettingUtil extends BaseConfig implements ModelSet
     }
 
     getLocalOptionGroups(settings: Settings) {
+        let models = chatboxAIModels
+        if (settings.language === 'zh-Hans' || settings.language === 'zh-Hant') {
+            models = [ ...chatboxAIModels, 'deepseek-chat', 'deepseek-reasoner' ]
+        }
         return [
             {
-                options: chatboxAIModels.map((value) => ({
+                options: models.map((value) => ({
                     label: this.formatModelLabel(value),
                     value: value,
                 })),
@@ -34,7 +39,25 @@ export default class ChatboxAISettingUtil extends BaseConfig implements ModelSet
     }
 
     formatModelLabel(value: string): string {
+        if (value === 'deepseek-chat') {
+            return 'DeepSeek V3'
+        } else if (value === 'deepseek-reasoner') {
+            return 'DeepSeek R1'
+        }
         return value.replace('chatboxai-', 'Chatbox AI ')
+    }
+
+    mergeOptionGroups(localOptionGroups: ModelOptionGroup[], remoteOptionGroups: ModelOptionGroup[]) {
+        const ret = [...remoteOptionGroups, ...localOptionGroups ]
+        const existedOptionSet = new Set<string>()
+        for (const group of ret) {
+            group.options = group.options.filter((option) => {
+                const existed = existedOptionSet.has(option.value)
+                existedOptionSet.add(option.value)
+                return !existed
+            })
+        }
+        return ret.filter((group) => group.options.length > 0)
     }
 
     selectSessionModel(settings: Session['settings'], selected: string): Session['settings'] {
