5772c3cdaae0ea08028a117ab1f339e000faf970
Bin-Huang <tohuangbin@gmail.com>
2025-03-03 21:44:10 +0800

    feat: 更换联系邮箱

--- src/renderer/packages/models/errors.ts
+++ src/renderer/packages/models/errors.ts
@@ -165,13 +165,13 @@ export class ChatboxAIAPIError extends BaseError {
             name: 'system_error',
             code: 20019,
             i18nKey:
-                'An error occurred while processing your request. Please try again later. If this error continues, please send an email to hi@benn.app for support.',
+                'An error occurred while processing your request. Please try again later. If this error continues, please send an email to hi@chatboxai.com for support.',
         },
         unknown: {
             name: 'unknown',
             code: 20020,
             i18nKey:
-                'An unknown error occurred. Please try again later. If this error continues, please send an email to hi@benn.app for support.',
+                'An unknown error occurred. Please try again later. If this error continues, please send an email to hi@chatboxai.com for support.',
         },
         model_not_support_web_browsing: {
             name: 'model_not_support_web_browsing',
