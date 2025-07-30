9e31d0cbc4f63f3ca8706cb17e2a531ca7cade2a
Ryan Wang <wtmanutd@163.com>
2025-07-30 16:54:58 +0800

    fix: capacitor sqlite keystore key generation failed (#355)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -25,7 +25,7 @@ const config: CapacitorConfig = {
         biometricAuth: false,
         biometricTitle: 'Biometric login for capacitor sqlite',
       },
-      androidIsEncryption: true,
+      androidIsEncryption: false,
       androidBiometric: {
         biometricAuth: false,
         biometricTitle: 'Biometric login for capacitor sqlite',
